240464096a313688958eaf4db65f44de043e6dac
Aleksandra Kolevska <aleksandrakolevska1@hotmail.com>
2017-12-09 23:03:05 +0100

    Dodaeni controlleri i ednostavno view za proverka na konekcijata so bazata

---
+++ Pizzeria/Pizzeria/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Pizzeria.Models;
+
+namespace Pizzeria.Controllers
+{
+    public class HomeController : Controller
+    {
+        PizzeriaEntities pizzaEntity;
+
+        public HomeController()
+        {
+            pizzaEntity = new PizzeriaEntities();
+        }
+        public ActionResult Index()
+        {
+            ViewData.Model = pizzaEntity.Tips.ToList();
+            ViewData.Model = (from c in pizzaEntity.Tips select c);
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
